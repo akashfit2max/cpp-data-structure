cba73693d2805c6c11e5d092e7c3e7591ef91131
akashfit2max <36466687+akashfit2max@users.noreply.github.com>
2019-12-15 10:56:39 +0530

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,4 +1,30 @@
 // program for stack implementation
+/*......................Definition – 
+Stack is a linear data structure which operates in a LIFO(Last In First Out) or  FILO (First In Last Out) pattern.
+It is named stack as it behaves like a real-world stack, for example – a deck of cards or a pile of plates, etc.
+Stack is an abstract data type with a bounded (predefined) capacity.
+It is a simple data structure that allows adding and removing elements in a particular order.
+The order may be LIFO(Last In First Out) or FILO(First In Last Out).
+stack data structure diagram
+
+Standard Stack Operations – 
+push() –  Place an item onto the stack. If there is no place for new item, stack is in overflow state.
+pop() – Return the item at the top of the stack and then remove it. If pop is called when stack is empty, it is in an underflow state.
+isEmpty() –  Tells if the stack is empty or not
+isfull() – Tells if the stack is full or not.
+peek() – Access the item at the i position
+count() – Get the number of items in the stack.
+change() – Change the item at the i position
+display() – Display all items in the stack
+Some Applications of Stack Data Structure –
+Balancing of symbols
+Infix to Postfix /Prefix conversion
+Redo-undo features at many places like editors, photoshop.
+Forward and backward feature in web browsers
+Used in many algorithms like Tower of Hanoi, tree traversals, stock span problem, histogram problem.
+Other applications can be Backtracking, Knight tour problem, rat in a maze, N queen problem and sudoku solver
+In Graph Algorithms like Topological Sorting and Strongly Connected Components.....................*/
+
 #include<iostream>
 #include<string>
 using namespace std;
